be446c463415cd1f3cc9a7291cf1ca3b946ad91f
kavitha565 <kavitha565@missionrnd.com>
2016-03-18 09:56:45 -0700

    Submit Commit

--- src/commonWords.cpp
+++ src/commonWords.cpp
@@ -12,11 +12,61 @@ NOTES: If there are no common words return NULL.
 */
 
 #include <stdio.h>
-
+#include<string.h>
 #include <malloc.h>
 
 #define SIZE 31
 
 char ** commonWords(char *str1, char *str2) {
-	return NULL;
+	if (str1 == NULL || str2 == NULL)
+		return NULL;
+
+	char tok_str1[100][100];
+	int words_count_1, words_count_2;
+	int i = 0;
+	char **output = (char**)malloc(100 * sizeof(char *));
+	int k = 0;
+	char tok_str2[100][100];
+	for (int j = 0; str1[j] != '\0'; j++){
+		if (str1[j] != ' ')
+			tok_str1[i][k++] = str1[j];
+		else{
+			i++;
+			k = 0;
+		}
+	}
+	words_count_1 = i;
+	i = 0; k = 0;
+
+	for (int j = 0; str2[j] != '\0'; j++){
+		if (str2[j] != ' ')
+			tok_str2[i][k++] = str2[j];
+		else{
+			i++;
+			k = 0;
+		}
+	}
+
+	words_count_2 = i;
+
+	int t = 0;
+	for (i = 0; i <= words_count_1; i++){
+
+		for (int j = 0; j <= words_count_2; j++){
+			//cout << tok_str1[i] << "   " << tok_str2[j];
+			if (strcmp(tok_str1[i], tok_str2[j]) == 0){
+				//cout << tok_str1[i];
+
+				output[t] = (char*)malloc(100 * sizeof(char));
+
+				output[t++] = tok_str1[i];
+			}
+		}
+	}
+
+	if (t == 0)
+		return NULL;
+	else
+		return output;
 }
+
